6cbde6e00097ae827bd677e88efd3e85d925c0ad
dune-official <lukasbaier@outlook.de>
2023-02-23 13:45:01 +0100

    Update clipboard.c

--- clipboard.c
+++ clipboard.c
@@ -1,98 +1,137 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+IPAM: 
+- Verbundwarte und evtl. Jürgens Adressen entfernen
+- andere, externe Adressen nachpflegen => Subnetz hinzufügen => 
 
-typedef char* string;
+Subnetz im CIDR laut FortiNet + Beschreibung "Internetnetz Standort ..." + VRF + im Wurzelordner
++ IP an sich anlegen (keine Gateways)
 
-void handle_input(int argc, char *restrict argv[], char *restrict char_argument, string restrict string_argument, int *restrict int_argument) {
-	int i;
-	for (i = 0; i < argc; i++) {
-		if (!strncmp(argv[i], "-i", 2) || !strncmp(argv[i], "--interface", 11)) {
-			 /* string argument */
-			if (i < argc-1 && argv[i+1][0] != '-') {
-				strncpy(string_argument, argv[i+1], strnlen(argv[i+1], 16));
-			} else {
-				fputs("Could not find valid interface, make sure it doesn't start with a dash\n", stderr);
-				exit(1);
-			}
-		} else if (!strncmp(argv[i], "-n", 2) || !strncmp(argv[i], "--number", 8)) {
-			/* int argument */
-			/* this is where it gets a little more complicated, we have to do some trickery */
-			if (i < argc-1 && argv[i+1][0] != '-') {
-				int_argument[0] = strtol(argv[i+1], NULL, 10);
-			} else {
-				fputs("Could not find valid count, make sure it doesn't start with a dash\n", stderr);
-				exit(1);
-			}
-		} else if (!strncmp(argv[i], "-m", 2) || !strncmp(argv[i], "--mode", 6)) {
-			/* char argument */
-			if (i < argc-1 && argv[i+1][0] != '-') {
-				strncpy(char_argument, argv[i+1], i);
-			} else {
-				fputs("Could not find valid mode, make sure it doesn't start with a dash\n", stderr);
-				exit(1);
-			}
-		}
-	}
+Router -> Static Route -> Gateway anlegen
 
-	if (0 == char_argument[0]) {
-		char_argument[0] = 'F';  // or what the hell ever the default should be
-	}
+Raisecom:
+Projektieren mit externem DHCP Server plus Script aus der Datenbank, per SSH mit den Geräten verbinden und automatisch Befehle absetzen
 
-	if (0 == string_argument[0]) {
-		strncpy(string_argument, "eth0", 4);
-	}
+def ip_to_bin(ip: str):
+    ip_split = ip.split(".")
+    ip_split_int = list(map(int, ip_split))
 
-	if (0 == int_argument[0]) {
-		int_argument[0] = 1;
-	}
-}
+    for index, element in enumerate(ip_split_int):
+        if element > 255:
+            raise NotImplementedError(
+                "Invalid IPv4 Address (Cannot be above 255)"
+            )
+        ip_split_int[index] = bin(element).split("b")[1].rjust(8, "0")
 
-int main(int argc, char *argv[]) {
-	/*
-	 * Requirements:
-	 * - At least one string, one char and one number (int)
-	 * - The string argument has a non defined length
-	 * - Arguments appear in random order
-	 * - Arguments are being signaled with a dash (for example: "-i" or "--interface")
-	 * - Error handling for when an argument is missing
-	 * - Default values for non-existing arguments
-	 * - Optionally, escape the quotes around a string, so it can safely start with a dash
-	 * - If possible, optimize, optimize, optimize!
-	 * */
+    return "".join(ip_split_int)
 
-	if (argc < 2) {
-		puts("Usage: ./main"
-			 "\n\t-i/--interface:\t\tSpecifies the network interface to use (Optional, default: \"eth0\""
-			 "\n\t-n/--number:\t\tSpecifies the amount of packages being sent (Optional, default: 1)"
-			 "\n\t-m/--mode:\t\tSpecifies the mode in which to send the package (Optional, default F)");
-		return 0;
-	}
 
-	int *int_argument = (int *) calloc(1, sizeof(*int_argument));
-	if (NULL == int_argument) {
-		fputs("Failed to initialize counter container\n", stderr);
-		exit(1);
-	}
-	int_argument[0] = 0;
+def bin_to_ip(bin_ip: str):
+    bin_ip = [bin_ip[i:i + 8] for i in range(0, len(bin_ip), 8)]
+    for index, element in enumerate(bin_ip):
+        bin_ip[index] = str(int(element, 2))
+    return ".".join(bin_ip)
 
-	char *char_argument = (char *) calloc(1, sizeof(*char_argument));
-	if (NULL == char_argument) {
-		fputs("Failed to initialize mode container\n", stderr);
-		exit(1);
-	}
-	char_argument[0] = 0x0;
 
-	string string_argument = (string) calloc(4, sizeof(*string_argument));
-	if (NULL == string_argument) {
-		fputs("Failed to initialize interface container\n", stderr);
-		exit(1);
-	}
-	string_argument[0] = 0x0;
+class NetworkMask:
 
-	handle_input(argc, argv, char_argument, string_argument, int_argument);
+    def __init__(self):
+        # the base ip address, often ending with a 0
+        self.network_address = None
 
-	printf("Sending %d package(s) to interface \"%s\" with mode '%c'\n", int_argument[0], string_argument, char_argument[0]);
+        # the amount of bits that are used to determine the host address
+        self.network_range = None
 
-	return 0;
-}
+        # the network mask in decimal notation used to determine the network address
+        self.network_mask = None
+
+        # the amount of devices fitting into this network (2^(32-network_range)-2)
+        self.device_amount = None
+
+        # the very first ip address available in this network
+        self.min_ip = None
+
+        # the very last ip address available in this network (not the broadcast address)
+        self.max_ip = None
+
+    @classmethod
+    def from_cidr(cls, base_ip: str, network_range: int):
+        nm = NetworkMask()
+        nm.network_range = network_range
+
+        binary_ip = ip_to_bin(base_ip)
+        network_address = binary_ip[:network_range].ljust(32, "0")
+
+        nm.min_ip = bin_to_ip(network_address[:31] + "1")
+        nm.max_ip = bin_to_ip(binary_ip[:network_range].ljust(31, "1") + "0")
+        nm.network_address = bin_to_ip(network_address)
+
+        nm.network_mask = bin_to_ip(("1" * network_range).ljust(32, "0"))
+        nm.device_amount = pow(2, 32 - network_range) - 2
+
+        return nm
+
+    @classmethod
+    def from_decimal(cls, base_ip: str, network_mask: str):
+        nm = NetworkMask()
+        nm.network_mask = network_mask
+
+        binary_ip = ip_to_bin(base_ip)
+        binary_range = ip_to_bin(network_mask)
+        set_bits = 0
+
+        for index, bit in enumerate(binary_range):
+            if bit == "1" and index != 0 and binary_range[index - 1] == "0":
+                raise NotImplementedError(
+                    "Invalid network range (Cannot be alternating 0 and 1"
+                )
+            if bit == "1":
+                set_bits += 1
+
+        network_address = binary_ip[:set_bits].ljust(32, "0")
+
+        nm.network_range = set_bits
+        nm.device_amount = pow(2, 32-set_bits) - 2
+        nm.min_ip = bin_to_ip(network_address[:31] + "1")
+        nm.max_ip = bin_to_ip(binary_ip[:set_bits].ljust(31, "1") + "0")
+        nm.network_address = bin_to_ip(network_address)
+
+        return nm
+
+    @classmethod
+    def from_range(cls, network_address: str, broadcast_address: str):
+        nm = NetworkMask()
+        nm.network_address = network_address
+        nm.max_ip = bin_to_ip(ip_to_bin(broadcast_address)[:31] + "0")
+
+        first_ip_bin_int = ip_to_bin(network_address)
+        broadcast_ip_bin_int = int(ip_to_bin(broadcast_address), 2)
+
+        nm.network_range = 32 - (len(bin(int(first_ip_bin_int, 2) ^ broadcast_ip_bin_int)) - 2)
+        nm.device_amount = pow(2, 32 - nm.network_range) - 2
+        nm.network_mask = bin_to_ip(("1" * nm.network_range).ljust(32, "0"))
+        nm.min_ip = bin_to_ip(first_ip_bin_int[:31] + "1")
+
+        return nm
+
+    def info(self):
+        return f"{self.network_address}/{self.network_range} ({self.network_mask})\n" \
+               f"Range: From {self.min_ip} to {self.max_ip} ({self.device_amount} devices)"
+
+    def __contains__(self, item: str):
+        item = ip_to_bin(item)[:self.network_range].ljust(32, "0")
+        return bin_to_ip(item) == self.network_address
+
+    def __str__(self):
+        return f"{self.network_address}/{self.network_range}"
+
+
+def ip_in_subnet(ip_to_check, subnet_info):
+    network_address = ip_to_bin(ip_to_check)[:subnet_info["network_range"]].ljust(32, "0")
+
+    if bin_to_ip(network_address) == subnet_info["network_address"]:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(NetworkMask.from_cidr("", 30).info())
