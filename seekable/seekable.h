202b81aa92a1990041629b01719b17b52d9e26b5
dune-official <baierl@dhbw-loerrach.de>
2023-07-06 22:27:02 +0200

    Create seekable.h

---
+++ seekable/seekable.h
@@ -0,0 +1,30 @@
+#ifndef SEEKABLE_SEEKABLE_H
+#define SEEKABLE_SEEKABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct SeekableNode {
+    void *object;
+    struct SeekableNode *next, *previous;
+};
+
+typedef struct SeekableWrapper {
+    struct SeekableNode *start, *current;
+} seek;
+
+extern seek *initialize();
+extern void *peek(seek *node);
+extern void *peek_right(seek *node);
+extern void *peek_left(seek *node);
+
+extern char seek_right(seek *node);
+extern char seek_left(seek *node);
+
+extern void set(void *restrict object, struct SeekableNode *restrict node);
+extern void set_right(void *restrict object, struct SeekableNode *restrict node);
+extern void set_left(void *restrict object, struct SeekableNode *restrict node);
+extern void insert_node_right(seek *node);
+extern void insert_node_left(seek *node);
+
+#endif
