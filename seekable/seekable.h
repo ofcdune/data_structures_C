ae561ba081b0c289bdc9cfcd3974daf199040e1a
dune-official <baierl@dhbw-loerrach.de>
2023-07-08 10:05:48 +0200

    Update seekable.h

--- seekable/seekable.h
+++ seekable/seekable.h
@@ -13,7 +13,7 @@ typedef struct SeekableWrapper {
     struct SeekableNode *start, *current;
 } seek;
 
-extern seek *initialize();
+extern seek *initialize_seekable();
 extern void *peek(seek *node);
 extern void *peek_right(seek *node);
 extern void *peek_left(seek *node);
@@ -21,7 +21,7 @@ extern void *peek_left(seek *node);
 extern char seek_right(seek *node);
 extern char seek_left(seek *node);
 
-extern void set(void *restrict object, struct SeekableNode *restrict node);
+extern void set_cuurent(void *restrict object, struct SeekableNode *restrict node);
 extern void set_right(void *restrict object, struct SeekableNode *restrict node);
 extern void set_left(void *restrict object, struct SeekableNode *restrict node);
 extern void insert_node_right(seek *node);
