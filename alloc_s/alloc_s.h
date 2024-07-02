1c86274d9ed8797ba5c41ea40616c2860ffe69c4
dune <baierl@dhbw-loerrach.de>
2024-07-02 14:05:50 +0200

    Update alloc_s.h
    Greatly simplified the process

--- alloc_s/alloc_s.h
+++ alloc_s/alloc_s.h
@@ -1,36 +1,12 @@
 #ifndef ALLOC_ALLOC_H
 #define ALLOC_ALLOC_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/* The names of the functions follow the naming conventions of printf_s, strncpy_s, etc. */
-
-void *malloc_s(size_t size, char *ErrorMessage) {
-	void *mem = malloc(size);
-	if (NULL == mem) {
-		fprintf(stderr,"Fatal error: %s", ErrorMessage);
-		exit(-1);
-	}
-	return mem;
-}
-
-void *calloc_s(size_t NumOfElements, size_t SizeOfElements, char *ErrorMessage) {
-	void *mem = calloc(NumOfElements, SizeOfElements);
-	if (NULL == mem) {
-		fprintf(stderr,"Fatal error: %s", ErrorMessage);
-		exit(-1);
-	}
-	return mem;
-}
-
-void *realloc_s(void *memory, size_t NewSize, char *ErrorMessage) {
-	void *mem = realloc(memory, NewSize);
-	if (NULL == mem) {
-		fprintf(stderr,"Fatal error: %s", ErrorMessage);
-		exit(-1);
-	}
-	return mem;
-}
+#define VALNUL(x) if (NULL == (x)) {fprintf(stderr, #x " in function %s is NULL, exiting!\n", __func__);exit(1);}
+#define NEW(type, name, amount) \
+    type *name = (type *) calloc(amount, sizeof(*name)); \
+    VALNUL(name)
 
 #endif
