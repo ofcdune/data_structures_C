c60ce9837560f8f0e99ec3caf8a9257781adf9da
dune <baierl@dhbw-loerrach.de>
2024-07-02 15:52:08 +0200

    Update alloc_s.h

--- alloc_s/alloc_s.h
+++ alloc_s/alloc_s.h
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 
 #define VALNUL(x) if (NULL == (x)) {fprintf(stderr, #x " in function %s is NULL, exiting!\n", __func__);exit(1);}
+
 #define NEW(type, name, amount) \
     type *name = (type *) calloc(amount, sizeof(*name)); \
     VALNUL(name)
+
+#define NEWSTRUCT(type, name) \
+    name = (type *) calloc(1, sizeof(*name)); \
+    VALNUL(name)
+
 
 #endif
