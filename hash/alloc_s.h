9083c1fee829d49d277bbc4b4237b29056df9e4a
dune <baierl@dhbw-loerrach.de>
2024-07-07 21:26:23 +0200

    Add files via upload

---
+++ hash/alloc_s.h
@@ -0,0 +1,17 @@
+#ifndef ALLOC_ALLOC_H
+#define ALLOC_ALLOC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VALNUL(x) if (NULL == (x)) {fprintf(stderr, #x " in function %s is NULL, exiting!\n", __func__);exit(1);}
+
+#define NEW(type, name, amount) \
+    type *name = (type *) calloc(amount, sizeof(*name)); \
+    VALNUL(name)
+
+#define NEWSTRUCT(type, name) \
+    name = (type *) calloc(1, sizeof(*name)); \
+    VALNUL(name)
+
+#endif
