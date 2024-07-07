74d4502c0e0d718077216ae66005a01a63a7e9fb
dune <baierl@dhbw-loerrach.de>
2024-07-07 21:26:04 +0200

    Update hash.h

--- hash/hash.h
+++ hash/hash.h
@@ -1,21 +1,31 @@
-#ifndef HASH_HASH_H
-#define HASH_HASH_H
+#ifndef HASHMAP_HASHMAP_H
+#define HASHMAP_HASHMAP_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct hash_node {
-	struct hash_node *next;
-	int key;
-	void *value;
-} hash;
+#include "alloc_s.h"
 
-/* This API works with integers only. A custom conversion function for different data types is required */
+struct hash_root {
+    unsigned int (*hashfktn)(unsigned int, unsigned int);
+    unsigned int bucket_size;
+    struct hash_node **bucket;
+};
 
-/* Gets a key */
-extern void *get(int key);
-/* Inserts a key and a value. Collisions will spawn another hash node  */
-extern void insert(int key, void *value);
-extern void delete(int key);
+struct hash_node {
+    unsigned int key;
+
+    void *value;
+    struct hash_node *next;
+};
+
+struct hash_root *hash_create_hashmap(unsigned int bucket_size);
+bool hash_insert(struct hash_root *restrict hashmap, unsigned int key, void *restrict value);
+void *hash_get(struct hash_root *restrict hashmap, unsigned int key);
+bool hash_exists(struct hash_root *restrict hashmap, unsigned int key);
+void internal_hash_delete(struct hash_node *restrict node_before);
+void *hash_delete(struct hash_root *restrict hashmap, unsigned int key);
+void hash_set_hashfunction(struct hash_root *restrict hashmap, unsigned int (*hashfktn)(unsigned int, unsigned int));
 
 #endif
